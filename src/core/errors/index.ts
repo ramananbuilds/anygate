b9a632ac1bf58db3be39c5bf7e8f92491a620c7b
ramanan-techlover <ramanan182004@gmail.com>
2026-07-24 07:00:18 +0530

    refactor(core): introduce src/core/ with constants, errors, events, and interfaces

---
+++ src/core/errors/index.ts
@@ -0,0 +1,1 @@
+export * from '../../shared/errors.js';
---
+++ src/core/index.ts
@@ -0,0 +1,4 @@
+export * as constants from './constants/index.js';
+export * as errors from './errors/index.js';
+export * as events from './events/index.js';
+export * as interfaces from './interfaces/index.js';
